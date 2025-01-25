82354d93960b49331302f55acb41b6258514ebda
Ignat Krushev <krushev@gmail.com>
2025-01-25 18:49:04 +0200

    main: Added cmus configuration

---
+++ cmus/.config/cmus/lib.pl
@@ -0,0 +1,133 @@
+/home/igi/Music/2019-09-01_10.17.42.ogg
+/home/igi/Music/2020-06-19 08.06.37.flac
+/home/igi/Music/AllegroVivace_AT/foreigner waiting for a gir like you.mp3
+/home/igi/Music/AllegroVivace_AT/Hank Williams Jr -I Just Ain't Been Able.mp3
+/home/igi/Music/AllegroVivace_AT/I will survive   Gloria gaynor www.dilandau.eu.mp3
+/home/igi/Music/AllegroVivace_AT/kVMElWitG8Wg.128.mp3
+/home/igi/Music/AllegroVivace_AT/michael buble - sway lyrics (256  kbps).mp3
+/home/igi/Music/AllegroVivace_AT/Sting - Desert Rose original song.mp3
+/home/igi/Music/Mantri/DISK1/Track 1.flac
+/home/igi/Music/Mantri/DISK1/Track 1.wav
+/home/igi/Music/Mantri/DISK1/Track 2.flac
+/home/igi/Music/Mantri/DISK1/Track 2.wav
+/home/igi/Music/Mantri/DISK1/Track 3.flac
+/home/igi/Music/Mantri/DISK1/Track 3.wav
+/home/igi/Music/Mantri/DISK1/Track 4.flac
+/home/igi/Music/Mantri/DISK1/Track 4.wav
+/home/igi/Music/Mantri/DISK1/Track 5.flac
+/home/igi/Music/Mantri/DISK1/Track 5.wav
+/home/igi/Music/Mantri/DISK1/Track 6.flac
+/home/igi/Music/Mantri/DISK1/Track 6.wav
+/home/igi/Music/Mantri/DISK1/Track 7.flac
+/home/igi/Music/Mantri/DISK1/Track 7.wav
+/home/igi/Music/Mantri/DISK1/Track 8.flac
+/home/igi/Music/Mantri/DISK1/Track 8.wav
+/home/igi/Music/Mantri/DISK1/Track 9.flac
+/home/igi/Music/Mantri/DISK1/Track 9.wav
+/home/igi/Music/Mantri/DISK2/Track 2.flac
+/home/igi/Music/Mantri/DISK2/Track 2.wav
+/home/igi/Music/Mantri/DISK2/Track 3.flac
+/home/igi/Music/Mantri/DISK2/Track 3.wav
+/home/igi/Music/Mantri/DISK2/Track 4.flac
+/home/igi/Music/Mantri/DISK2/Track 4.wav
+/home/igi/Music/Mantri/DISK2/Track 5.flac
+/home/igi/Music/Mantri/DISK2/Track 5.wav
+/home/igi/Music/Mantri/Track 3.flac
+/home/igi/Music/Mantri/Track 5.flac
+/home/igi/Music/Mantri/Track 8.flac
+/home/igi/Music/MonroInstitute/01-SBVR_Release_Revitalize.wav
+/home/igi/Music/MonroInstitute/02-SBVR_Heart_Connection.wav
+/home/igi/Music/MonroInstitute/03-SBVR_Spoon_Bending_Without_Force.wav
+/home/igi/Music/MonroInstitute/04-SBVR_Manifesting_For_Physical_Emotional_Mental_Mixdown_1.wav
+/home/igi/Music/Tina Turner - Nam Myoho Renge Kyo (2H Buddhist Mantra)-128.mp3
+/home/igi/Music/Warriyo - Mortals (feat. Laura Brehm).mp3
+/home/igi/Music/Bird.mp3
+/home/igi/Music/MonroInstitute/01-SBVR  Release and Revitalize.mp3
+/home/igi/Music/MonroInstitute/02-SBVR  Heart Connection.mp3
+/home/igi/Music/MonroInstitute/03-SBVR  Spoon Bending Without Force.mp3
+/home/igi/Music/03.flac
+/home/igi/Music/01.flac
+/home/igi/Music/02.flac
+/home/igi/Music/MonroInstitute/04-SBVR  Manifesting for Physical Emotional Mental Mixdown 1.mp3
+/home/igi/Music/Mantri/DISK1/Track 1.mp3
+/home/igi/Music/Mantri/DISK1/Track 2.mp3
+/home/igi/Music/Mantri/DISK1/Track 3.mp3
+/home/igi/Music/Mantri/DISK1/Track 4.mp3
+/home/igi/Music/Mantri/DISK1/Track 5.mp3
+/home/igi/Music/Mantri/DISK1/Track 6.mp3
+/home/igi/Music/Mantri/DISK1/Track 7.mp3
+/home/igi/Music/Mantri/DISK1/Track 8.mp3
+/home/igi/Music/Mantri/DISK1/Track 9.mp3
+/home/igi/Music/Mantri/DISK2/Track 2.mp3
+/home/igi/Music/Mantri/DISK2/Track 3.mp3
+/home/igi/Music/Mantri/DISK2/Track 4.mp3
+/home/igi/Music/Mantri/DISK2/Track 5.mp3
+/home/igi/Music/Mantri/Track 3.mp3
+/home/igi/Music/Mantri/Track 5.mp3
+/home/igi/Music/Mantri/Track 8.mp3
+/home/igi/Music/basta_na_zare.mp3
+/home/igi/Music/Bab' Azîz/CD2/04. Abadan Wife - Percussion.mp3
+/home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle - 01 - Aas Guray Nameh (Mangalacharan Mantra).flac
+/home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle - 02 - Gobindah, Gobindah, Gobindah, Haray, Haray.flac
+/home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle - 03 - Waah Yantee.flac
+/home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle - 04 - Guru Ram Das Miracle Mantra.flac
+/home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle - 05 - Mool Mantra.flac
+/home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle - 06 - Ong Namo Guru Dev Namo (Adi Mantra).flac
+/home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle - 07 - The Longtime Sun Shine Song.flac
+/home/igi/Music/Amrit Kirtan/01 Aad guray Nahme.mp3
+/home/igi/Music/Amrit Kirtan/02 Gobindah, Gobindah, Gobindah, Harey, Harey.mp3
+/home/igi/Music/Amrit Kirtan/03 Waah Yantee.mp3
+/home/igi/Music/Amrit Kirtan/04 Guru Ram Das Miracle Mantra.mp3
+/home/igi/Music/Amrit Kirtan/05 Mool Mantra.mp3
+/home/igi/Music/Amrit Kirtan/06 Ong Namo Guru Dev Namo.mp3
+/home/igi/Music/Amrit Kirtan/07 May the Longtime.mp3
+cue:///home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle.cue/1
+cue:///home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle.cue/2
+cue:///home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle.cue/3
+cue:///home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle.cue/4
+cue:///home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle.cue/5
+cue:///home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle.cue/6
+cue:///home/igi/Music/Amrit Kirtan - Sacred Circle (2007)/Amrit Kirtan - Sacred Circle.cue/7
+/home/igi/Music/chuhal/XC167697-Petit.Duc-SP12.Ext-CF09p59.mp3
+/home/igi/Music/Bab' Azîz/CD2/06. Armand Amar - Woman In a Mosque - Ya Allah.mp3
+/home/igi/Music/Bab' Azîz/1. Maryam.mp3
+/home/igi/Music/Bab' Azîz/2. Bab' Azîz.mp3
+/home/igi/Music/Bab' Azîz/3. Poêm of the Butterflies.mp3
+/home/igi/Music/Bab' Azîz/4. Salvacci Denducci Pace.mp3
+/home/igi/Music/Bab' Azîz/5. Zikr.mp3
+/home/igi/Music/Bab' Azîz/6. Poêm of the Atoms I.mp3
+/home/igi/Music/Bab' Azîz/7. You Created the Night I Made the Light.mp3
+/home/igi/Music/Bab' Azîz/8. Ishtar.mp3
+/home/igi/Music/Bab' Azîz/9. Song of the Red Dervish.mp3
+/home/igi/Music/Bab' Azîz/10. Poêm of the Atoms II.mp3
+/home/igi/Music/Bab' Azîz/11. Dream of the Palace.mp3
+/home/igi/Music/Bab' Azîz/12. The Wedding.mp3
+/home/igi/Music/Bab' Azîz/CD2/05. Armand Amar & Ayay E Del - Satara's Solo.mp3
+/home/igi/Music/Bab' Azîz/CD2/10. Ayay E Del - Ayay E Del-Mast E Kalandar.mp3
+/home/igi/Music/Bab' Azîz/CD2/02. Aziz Mian - Ya Habib.mp3
+/home/igi/Music/Bab' Azîz/CD2/09. Bab-ı Aziz Ashigh Hay E Tabriz - Song To The Prophet.mp3
+/home/igi/Music/AllegroVivace_AT/Beach Boys - Surfin Usa HD [LoudTronix] [SQ].mp3
+/home/igi/Music/AllegroVivace_AT/lGeYiaaBfpqionqLo6ZkoqN7oKeipm2MoYxlnqK@@@wx.mp3
+/home/igi/Music/AllegroVivace_AT/Bob Marley & The Wailers - 01 - Is This Love.mp3
+/home/igi/Music/Bab' Azîz/CD2/12. Divana.mp3
+/home/igi/Music/AllegroVivace_AT/Lets call the whole thing off.mp3
+/home/igi/Music/AllegroVivace_AT/11-Eurythmics-SweetDreams.mp3
+/home/igi/Music/11 - Exotica - Une Miss s'immisce.mp3
+/home/igi/Music/chuhal/XC134560-2000051012_Otus scops_M_Turquie_F DEROUSSEN.mp3
+/home/igi/Music/chuhal/Otus scops_Rende_4_aprile 2012_.mp3
+/home/igi/Music/11 - Exotica - Une Miss s'immisce.flac
+/home/igi/Music/Bab' Azîz/CD2/03. Ghazal Shakeri - Man O To.mp3
+/home/igi/Music/Bab' Azîz/CD2/08. Armand Amar & Ghazal Shakeri - Song Of The Red Dervish.mp3
+/home/igi/Music/chuhal/XC144994-assiolo84.mp3
+/home/igi/Music/miravi-volya-mp3.mp3
+/home/igi/Music/Bab' Azîz/CD2/07. Mohammed Azald - Ayan Gorundi.mp3
+/home/igi/Music/chuhal/chuhal.mp3
+/home/igi/Music/chuhal/XC177732-Zwergohreule_Gesang_NSG Ehemalige Klärteiche Bedburg_26.04.2014_LS_50139.mp3
+/home/igi/Music/operskoy-zov-slovno-veter-v-stepi-file.mp3
+/home/igi/Music/chuhal/XC120449-06291_pfranke_110516_zoeule_pulchellus_1PA_DG_A.mp3
+/home/igi/Music/AllegroVivace_AT/queen champions.mp3
+/home/igi/Music/AllegroVivace_AT/right here waiting.mp3
+/home/igi/Music/AllegroVivace_AT/beyound the sea.mp3
+/home/igi/Music/Bab' Azîz/CD2/01. Salar Aghili - You Created The Night, I Made The Lamp.mp3
+/home/igi/Music/Bab' Azîz/CD2/11. Salar Aghil - Tasnife Eragh (Asheghan).mp3
+/home/igi/Music/AllegroVivace_AT/Stevie Wonder - Superstition (mp3li2.com).mp3
