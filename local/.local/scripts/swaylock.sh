0672b20a350c804ce85be755de6da13df21f5a97
Ignat Krushev <krushev@gmail.com>
2026-02-28 15:35:27 +0200

    main: swaylock fix

--- local/.local/scripts/swaylock.sh
+++ local/.local/scripts/swaylock.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-swaylock -f -e -c 000000 --image $(find "$HOME/.config/backgrounds/" -type f | shuf -n 1)
-#swaylock -f -e -c 000000 --image "$(find ~/.config/backgrounds/ -type f | shuf -n 1)"
+swaylock -e -c 000000 --image "$(find ~/.config/backgrounds/ -type f | shuf -n 1)"
