6f87609c2a72f483317ec8f5753a94da7e898705
Ignat Krushev <krushev@gmail.com>
2026-02-28 14:52:22 +0200

    main: swayloch, hyprlock stasis cinfig changes

--- local/.local/scripts/swaylock.sh
+++ local/.local/scripts/swaylock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-swaylock -f -e -c 000000 --image "$(find ~/.config/backgrounds/ -type f | shuf -n 1)"
+swaylock -f -e -c 000000 --image $(find "$HOME/.config/backgrounds/" -type f | shuf -n 1)
+#swaylock -f -e -c 000000 --image "$(find ~/.config/backgrounds/ -type f | shuf -n 1)"
